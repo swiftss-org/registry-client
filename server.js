2f453607d4a7be661f91d20b0aa1c18993e68d3b
whosyourtaco <george.metakos@orfium>
2021-06-23 18:04:47 +0300

    feat: update package.json

---
+++ server.js
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require('express');
+const path = require('path');
+const serverPort = process.env.PORT || 3000;
+const environment = process.env.NODE_ENV || 'development';
+
+// construct a mini server
+const app = express();
+
+// we force a redirect to "https://" only during production
+if (environment === 'production') {
+  app.use(function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(`https://${req.hostname}${req.url}`);
+    }
+    next();
+  });
+}
+
+// allow static assets to be served from the /build folder. Only HTML will be served from there,
+// the rest will be served from the CDN
+app.use(express.static(path.join(path.resolve(), 'build')));
+
+// resolve all requests to the index.html file
+// eslint-disable-next-line
+app.get('*', function (req, res) {
+  res.sendFile(path.join(path.resolve(), 'build/index.html'));
+});
+
+// initialize server
+app.listen(serverPort, function () {
+  console.log(`Listening on port ${serverPort}`);
+});
