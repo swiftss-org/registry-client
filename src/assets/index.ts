2e97afb6ec1cabfe5f7e0d1d7430778657f7fa12
paolo7 <paolo.pareti@gmail.com>
2024-05-01 17:03:32 +0100

    Added site localisation for title, header and footer text, and for the main icon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,15 @@
+import GHLogo from 'assets/gh-logo.png'
+import SLLogo from 'assets/sl-logo.png';
 import SwiftSSLogo from 'assets/swiftss-logo.png';
+import TSALogoLocal from 'assets/tsa-logo-local.png';
+import TSALogoTest from 'assets/tsa-logo-test.png';
 import TSALogo from 'assets/tsa-logo.png';
 
 export default {
+  GHLogo,
+  SLLogo,
   TSALogo,
   SwiftSSLogo,
+  TSALogoLocal,
+  TSALogoTest,
 };
