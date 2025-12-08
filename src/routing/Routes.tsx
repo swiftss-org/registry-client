9212f54650f46866198a892854bad446d122cc11
Sabi <szigyi.szogyi@gmail.com>
2025-12-08 20:14:07 +0000

    restrict the globalKPI page to admin level as it was before

--- src/routing/Routes.tsx
+++ src/routing/Routes.tsx
@@ -17,40 +17,27 @@ import { __TOKEN__ } from '../utils/constants';
 import { getUserStorageItem } from '../utils/storage';
 
 const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
-  const token = getUserStorageItem(__TOKEN__);
-  return token ? <>{children}</> : <Navigate to={urls.login()} />;
+    const token = getUserStorageItem(__TOKEN__);
+    const level = getUserStorageItem('user_level');
+    if (!token) {
+        return <Navigate to={urls.login()} />;
+    }
+    if (rest.path === '/' || !Component) {
+      return <Navigate to={urls.patients()} />;
+    }
+    if (rest.path === '/nationalKPIs' && level !== 'NATIONAL_LEAD') {
+      return <Navigate to={urls.landingPage()} />; // redirect unauthorized users
+    }
+    return <>{children}</>;
 };
 
-// import React from 'react';
-//
-// import Layout from 'pages/Layout/Layout';
-// import { Redirect, Route } from 'react-router-dom';
-//
-// import { CustomRouteProps } from './types';
-// import urls from './urls';
-// import { __TOKEN__ } from '../utils/constants';
-// import { getUserStorageItem } from '../utils/storage';
-//
-// const PrivateRoute: React.FC<CustomRouteProps> = ({ component: Component, ...rest }) => {
-//   const token = getUserStorageItem(__TOKEN__);
-//   const level = getUserStorageItem('user_level');
-//
-//   if (!token) {
-//     return <Route {...rest} render={() => <Redirect to={urls.login()} />} />;
-//   }
-//   if (rest.path === '/' || !Component) {
-//     return <Redirect to={urls.patients()} />;
-//   }
-//
-//   if (rest.path === '/nationalKPIs' && level !== 'NATIONAL_LEAD') {
-//     return <Redirect to={urls.landingPage()} />; // redirect unauthorized users
-//   }
-//
-//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//   return <Route {...rest} render={(props: any) => <Layout component={Component} {...props} />} />;
-// };
-//
-// export default PrivateRoute;
+const AdminRoute = ({ children }: { children: React.ReactNode }) => {
+  const token = getUserStorageItem(__TOKEN__);
+  const isStaff = getUserStorageItem('is_staff') === 'true';
+  const isSuperuser = getUserStorageItem('is_superuser') === 'true';
+  const username = getUserStorageItem('username');
+  return token && ((isStaff || isSuperuser) && username == 'admin') ? <>{children}</> : <Navigate to={urls.login()} />;
+};
 
 const router = createBrowserRouter([
   {
