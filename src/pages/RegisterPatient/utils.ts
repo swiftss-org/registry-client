89baa264ededf8742f26e1a140ac8b5a4898203c
Sabi <szigyi.szogyi@gmail.com>
2026-03-05 11:50:19 +0000

    remove redundant form validation functions
    the real validations are stored with the Forms

--- src/pages/RegisterPatient/utils.ts
+++ src/pages/RegisterPatient/utils.ts
@@ -1,48 +1,9 @@
-import { FilterOption } from '../types';
-import { RegisterPatientFormType } from './types';
 import { HospitalsAPI } from '../../models/apiTypes';
-
-const REQUIRED_FIELD_MSG = 'This field is required';
+import { FilterOption } from '../types';
 
 export const getHospitalOptions = (hospitals: HospitalsAPI[]): FilterOption[] => {
   return hospitals.map((hospital) => ({
     label: hospital?.name ?? '',
     value: hospital?.id,
   }));
-};
-
-export const patientFormValidation = (values: RegisterPatientFormType) => {
-  const errors: Partial<Record<keyof RegisterPatientFormType, string>> = {};
-
-  if (!values.hospital && typeof values.hospital !== 'object') {
-    errors.hospital = REQUIRED_FIELD_MSG;
-  }
-
-  if (!values.firstName?.trim()) {
-    errors.firstName = REQUIRED_FIELD_MSG;
-  }
-
-  if (!values.lastName?.trim()) {
-    errors.lastName = REQUIRED_FIELD_MSG;
-  }
-
-  if (!values.yearOfBirth) {
-    errors.yearOfBirth = REQUIRED_FIELD_MSG;
-  }
-
-  if (!values.gender) {
-    errors.gender = REQUIRED_FIELD_MSG + '. Please select the gender above.';
-  }
-
-  if (!values.patientHospitalId) {
-    errors.patientHospitalId = REQUIRED_FIELD_MSG;
-  }
-
-  if (!values.phone1) {
-    errors.phone1 = REQUIRED_FIELD_MSG;
-  } else if (!/^\d+$/.test(values.phone1)) {
-    errors.phone1 = 'Phone #1 field must be a number';
-  }
-
-  return errors;
 };
